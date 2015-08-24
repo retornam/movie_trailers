4f2bfe1cb88bd4312358f44bbcccbc9b71f5593f
Edem Kumodzi <ekumodzi@LAekumodzi.local>
2015-08-24 03:29:18 -0500

    Made changes to code to respect python formatting guidelines

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-To run this project, simply execute the command "python entertainment_center.py"
-This will create the movies and call the fresh_tomatoes utilities to dynamically generate the html page.
+# Movie Trailer Website
+A dynamically generated website from a list of given movies using python.
+
+## How to Run
+Execute the following command
+
+    python entertainment_center.py
+
+This will generate an html file called fresh_tomatoes.html and load it in the
+default browser
+
+## Notes
+
+The Movie class has an extra property for storing movie summaries.
+
+The module fresh_tomatoes.py was modified a bit to handle adding movies
+summaries to each trailer modal.
--- media.py
+++ media.py
@@ -1,10 +1,25 @@
+# Movie class to be used for creating instances of movies. Contains title
+# poster image link, trailer link and summary
 class Movie():
-	title = "title"
-	poster_image_url = "poster"
-	trailer_youtube_url = "trailer"
-	movie_summary = "summary"
-	def __init__(self, title, poster_image_url, trailer_youtube_url, movie_summary):
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
-		self.movie_summary = movie_summary
+    """Movie structure with a title, a poster image link,
+    a trailer link and a summary"""
+
+    # movie title, by default will be set to 'title'
+    title = "title"
+
+    # movie poster image url, by default will be set to 'http://fake.jpg'
+    poster_image_url = "http://fake.jpg"
+
+    # movie trailer url, by default will be set to 'http://fake.mov'
+    trailer_youtube_url = "http://fake.mov"
+
+    # movie summary
+    movie_summary = "summary"
+
+    # constructor to instantiate new movies and pass arguments directly
+    def __init__(self, title, poster_image_url, trailer_youtube_url,
+                 movie_summary):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
+        self.movie_summary = movie_summary
